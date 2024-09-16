a69ce160c30f0e67c847e1b9be0fef435543f1ab
Kevin Rasmusson <kevin@jkmholding.com>
2024-09-16 19:39:58 +0200

    SEC-34 added auth middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
-export const authMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    // Do something
+export interface Session {
+    jwt?: string;
+    refreshToken?: string;
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            session?: Session;
+        }
+    }
+}
+
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    const token = req.session?.jwt;
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET as string);
+
+    if (!verified) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
     next();
 }
